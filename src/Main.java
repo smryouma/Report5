5475d6627ae885ee1313ad054f501612aa910d01
smryouma <e225745@ie.u-ryukyu.ac.jp>
2022-12-17 17:13:55 +0900

    ステップ２

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-    String str =null;
-    System.out.println(str.length());
+        try{
+            String str =null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }
+    
     }
 }
